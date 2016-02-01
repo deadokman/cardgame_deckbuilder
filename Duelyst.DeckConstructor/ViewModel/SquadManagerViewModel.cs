b307df52b2b42079b155ba4978f230930032fb7b
NikitaStepanov <deadokman@yandex.ru>
2016-02-01 21:38:45 +0500

    Работа модели формирования отряда

--- Duelyst.DeckConstructor/ViewModel/SquadManagerViewModel.cs
+++ Duelyst.DeckConstructor/ViewModel/SquadManagerViewModel.cs
@@ -4,8 +4,10 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Duelyst.DeckConstructor.ViewModel.DeckCardItem;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
 
 namespace Duelyst.DeckConstructor.ViewModel
 {
@@ -14,9 +16,41 @@ namespace Duelyst.DeckConstructor.ViewModel
         public SquadManagerViewModel()
         {
             CardListItems = new ObservableCollection<CardListItemViewModelBase>();
+            CardCollectionMode = true;
+            NewSquadCommand = new RelayCommand(EnterSquadBuilderMode);
         }
 
-        public ObservableCollection<CardListItemViewModelBase> CardListItems { get; set; } 
+        public ObservableCollection<CardListItemViewModelBase> CardListItems { get; set; }
+
+        /// <summary>
+        /// Команда переводящая вью модель в режим построение отряда
+        /// </summary>
+        public ICommand NewSquadCommand { get; set; }
+
+        private void EnterSquadBuilderMode()
+        {
+            CardCollectionMode = false;
+
+        }
+
+        private void BuildSquadCollectionItems()
+        {
+            
+        }
+
+        private void TryAddCardToSquad()
+        {
+            
+        }
+
+        public bool SquadBuilderMode
+        {
+            get
+            {
+                return !_cardCollectionObserverMod;
+
+            }
+        }
 
         public bool CardCollectionMode
         {
@@ -29,6 +63,7 @@ namespace Duelyst.DeckConstructor.ViewModel
             {
                 _cardCollectionObserverMod = value;
                 RaisePropertyChanged(() => CardCollectionMode);
+                RaisePropertyChanged(() => SquadBuilderMode);
             }
         }
         private bool _cardCollectionObserverMod;
